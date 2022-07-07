d277c36d430bfaf5f10d2413da82fe8e03de1a82
Bian Yu Wang <bianyu.wang@gmail.com>
2022-07-07 11:50:45 -0400

    add counter method finished

--- BlazorWithRedux/Store/CounterReducer.cs
+++ BlazorWithRedux/Store/CounterReducer.cs
@@ -13,7 +13,7 @@ namespace BlazorWithRedux.Store
         {
             return state with
             {
-                Count = state.Count 
+                Count = state.Count +1
             };
 
         }
